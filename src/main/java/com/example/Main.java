e230734ac8f9dea3b4ea43c439e64f4032e9c921
Otubusin Ademuyiwa (Innovate. Educate. Elevate.) <53128146+tay4real@users.noreply.github.com>
2025-09-03 15:11:04 +0100

    Update

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,13 +1,16 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
   public static void main(String[] args) {
 
     Integer[] scores = new Integer[]{80, 66, 73, 92, 43};
+      Stream<Integer> scoresStream = Arrays.stream(scores);
 
     List<String> shoppingList = new ArrayList<>();
     shoppingList.add("coffee");
@@ -15,6 +18,13 @@ public class Main {
     shoppingList.add("pineapple");
     shoppingList.add("milk");
     shoppingList.add("pasta");
+    Stream<String> shoppingListStream = shoppingList.stream();
+    shoppingListStream.sorted()
+            .map(item -> item.toUpperCase())
+            .filter(item -> item.startsWith("P"))
+            .forEach(item -> System.out.println(item));
+
+    Stream<String> lettersStream = Stream.of("a", "b", "c");
 
   }
 }
